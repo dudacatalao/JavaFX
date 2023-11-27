875c5830e67a578d4b991c205a680e2f88791f40
dudacatalao <dmelocatalao@gmail.com>
2023-11-27 11:22:06 -0300

    new

---
+++ untitled/src/controller/inicial.java
@@ -0,0 +1,10 @@
+package controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class inicial extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
